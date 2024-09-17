c715960d722267ff3e29a470773beed1360b8f40
Shobhit <shobhita@andrew.cmu.edu>
2024-09-17 18:42:36 -0400

    25hz and consistent across devices

--- serial_to_ft/src/multi_fts_serial_node.cpp
+++ serial_to_ft/src/multi_fts_serial_node.cpp
@@ -10,6 +10,14 @@
 #include "read_serial2.hpp"
 #include <geometry_msgs/WrenchStamped.h>
 
+enum Sensor
+{
+    DESTROYER,
+    ARCHITECT,
+    BUILDER,
+    CREATOR
+};
+
 class FTInterface
 {
 public:
@@ -20,15 +28,16 @@ public:
     short sts;            // return value from functions
     float *TT;
 
-    FTInterface(char *calfilepath, unsigned short index = 1)
+    FTInterface(std::string calfile, unsigned short index = 1)
     {
+        char *calfilepath = const_cast<char *>(calfile.data());
         this->calfilepath = calfilepath;
         this->index = index;
         this->cal = createCalibration(calfilepath, index);
-
         if (cal == NULL)
         {
-            std::cerr << "\nSpecified calibration could not be loaded.\n";
+            std::cerr << "\nSpecified calibration could not be loaded: "<< calfile<< "\n";
+	    std::cerr << calfilepath <<"\n";
             exit(1);
         }
 
@@ -68,49 +77,6 @@ public:
             exit(1);
         }
 
-        // Set tool transform.
-        // This line is only required if you want to move or rotate the sensor's coordinate system.
-        // This example tool transform translates the coordinate system 20 mm along the Z-axis
-        // and rotates it 45 degrees about the X-axis.
-
-        /*
-    float temp_TT[] = {0,0,20,45,0,0};
-    TT = temp_TT;
-        sts = SetToolTransform(cal, TT, "mm", "degrees");
-        switch (sts)
-        {
-        case 0:
-            break; // successful completion
-        case 1:
-            std::cerr<<"Invalid Calibration struct";
-            exit(1);
-        case 2:
-            std::cerr<<"Invalid distance units";
-            exit(1);
-        case 3:
-            std::cerr<<"Invalid angle units";
-            exit(1);
-        default:
-            std::cerr<<"Unknown error";
-            exit(1);
-        }
-        */
-        // Temperature compensation is on by default if it is available.
-        // To explicitly disable temperature compensation, uncomment the following code
-        /*SetTempComp(cal,FALSE);                   // disable temperature compensation
-        switch (sts) {
-            case 0: break;	// successful completion
-            case 1: printf("Invalid Calibration struct"); return 0;
-            case 2: printf("Temperature Compensation not available on this transducer"); return 0;
-            default: printf("Unknown error"); return 0;
-        }*/
-
-        /*
-        float SampleBias[7] = {0.2651, -0.0177, -0.0384, -0.0427, -0.1891, 0.1373, -3.2423};
-
-        // store an unloaded measurement; this removes the effect of tooling weight
-        Bias(cal, SampleBias);
-        */
     }
 
     void setToolTransform(float TT[])
@@ -154,29 +120,50 @@ public:
     ros::Publisher pub;
     ros::Publisher pub_raw;
 
-    FTSensor(char *name, ros::NodeHandle nh) : fti(selectCalFile(name))
+    FTSensor(enum Sensor index, ros::NodeHandle nh) : fti(selectCalFile(index))
     {
-        std::stringstream ss;
-        ss << name << "/fts";
-        std::string pub_topic = ss.str();
-        ss.str("");
-        ss << name << "/fts_raw";
-        std::string pub_raw_topic = ss.str();
-        pub = nh.advertise<geometry_msgs::WrenchStamped>(pub_topic, 1000);
+        std::string pub_topic;
+        std::string pub_raw_topic;
+	std::stringstream ss1, ss2;
+
+	switch(index){
+		case ARCHITECT:
+			ss1 << "yk_architect" << "/fts";
+			ss2 << "yk_architect" << "/fts_raw";		
+			break;
+		case BUILDER:
+			ss1 << "yk_builder" << "/fts";
+			ss2 << "yk_builder" << "/fts_raw";		
+			break;
+		case CREATOR:
+			ss1 << "yk_creator" << "/fts";
+			ss2 << "yk_creator" << "/fts_raw";		
+			break;
+		case DESTROYER:
+			ss1 << "yk_destroyer" << "/fts";
+			ss2 << "yk_destroyer" << "/fts_raw";		
+			break;
+
+	}
+
+        pub_topic = ss1.str();
+        pub_raw_topic = ss2.str();
+        
+	pub = nh.advertise<geometry_msgs::WrenchStamped>(pub_topic, 1000);
         pub_raw = nh.advertise<geometry_msgs::WrenchStamped>(pub_raw_topic, 1000);
     }
 
 private:
-    static char *selectCalFile(char *name)
+    static std::string selectCalFile(enum Sensor index)
     {
 	std::string calfile;
-        if (strcmp(name, "yk_architect") == 0)
+        if (index == Sensor::ARCHITECT)
             calfile = "FT45384_r1.cal";
-        else if (strcmp(name, "yk_builder") == 0)
+	else if (index == Sensor::BUILDER)
             calfile = "FT45383_r1.cal";
-        else if (strcmp(name, "yk_creator") == 0)
+	else if (index == Sensor::CREATOR)
             calfile = "FT45386.cal";
-        else if (strcmp(name, "yk_destroyer") == 0)
+	else if (index == Sensor::DESTROYER)
             calfile = "FT45385.cal";
         else
         {
@@ -184,18 +171,11 @@ private:
             exit(1);
         }
 
-        std::string calfilepath = "/home/mfi/repo/ros_ws/src/fts_serial/serial_to_ft/config/" + calfile;
+        std::string calfilepath = "/home/mfi/repo/ros1_ws/src/fts_serial/serial_to_ft/config/" + calfile;
         char *file = const_cast<char *>(calfilepath.data());
-        return file;
+	
+        return calfilepath;
     }
-};
-
-enum Sensor
-{
-    ARCHITECT,
-    BUILDER,
-    CREATOR,
-    DESTROYER
 };
 
 int main(int argc, char *argv[])
@@ -205,22 +185,26 @@ int main(int argc, char *argv[])
 
     int baud_arg;
     ros::param::param<int>("~baudrate", baud_arg, 115200);
-    ros::Rate loop_rate(1000);
+    ros::Rate loop_rate(500);
 
     std::string data;
-    enum Sensor device;
     float voltages[6];
     float FT[6];
     geometry_msgs::WrenchStamped msg;
     geometry_msgs::WrenchStamped msg_raw;
-    FTSensor fts[4] = {FTSensor("yk_architect", nh), FTSensor("yk_builder", nh), FTSensor("yk_creator", nh), FTSensor("yk_destroyer", nh)};
+    int index = 0;
+    FTSensor fts[4] = {FTSensor((Sensor)index, nh), FTSensor((Sensor)++index, nh), FTSensor((Sensor)++index, nh), FTSensor((Sensor)++index, nh)};
 
     // read usb srt values
     SerialPortReader reader("/dev/ttyUSB0", baud_arg);
+
+    std::cout<<"\nSetup done\n";
+
     while (ros::ok())
     {
         data = reader.readValues();
         std::stringstream ss(data);
+    	enum Sensor device=static_cast<Sensor>(9);
         std::string temp;
 
         if (!data.empty())
@@ -231,41 +215,51 @@ int main(int argc, char *argv[])
 
             // Expecting data in the form of "V: 1 0.00, 0.00, 0.00, 0.00, 0.00, 0.00"
             ss >> temp;
-            while (temp != "V:" && !ss.eof())
+            while (temp != "V" && !ss.eof())
                 ss >> temp;
-
-            if (temp == "V:")
+            
+	    if (temp == "V")
+	    {
+                //ss >> id >> voltages[0] >> temp >> voltages[1] >> temp >> voltages[2] >> temp >> voltages[3] >> temp >> voltages[4] >> temp >> voltages[5];
                 ss >> id >> voltages[0] >> voltages[1] >> voltages[2] >> voltages[3] >> voltages[4] >> voltages[5];
                 device = static_cast<Sensor>(id);
+   	        //std::cout<<"I got values for "<<id<<std::endl;
+	    }
         }
 
-        fts[device].fti.getFTvalues(FT, voltages);
+	if (device < 4 && device > -1)
+	{
+		//std::cout<<"\nGot Voltages from "<<device<<"\n";
+		fts[device].fti.getFTvalues(FT, voltages);
+		//std::cout<<"....";
+		msg.header.stamp = ros::Time::now();
+		msg.wrench.force.x = FT[0];
+		msg.wrench.force.y = FT[1];
+		msg.wrench.force.z = FT[2];
+		msg.wrench.torque.x = FT[3];
+		msg.wrench.torque.y = FT[4];
+		msg.wrench.torque.z = FT[5];
 
-        msg.header.stamp = ros::Time::now();
-        msg.wrench.force.x = FT[0];
-        msg.wrench.force.y = FT[1];
-        msg.wrench.force.z = FT[2];
-        msg.wrench.torque.x = FT[3];
-        msg.wrench.torque.y = FT[4];
-        msg.wrench.torque.z = FT[5];
+		msg_raw.header.stamp = ros::Time::now();
+		msg_raw.wrench.force.x = voltages[0];
+		msg_raw.wrench.force.y = voltages[1];
+		msg_raw.wrench.force.z = voltages[2];
+		msg_raw.wrench.torque.x = voltages[3];
+		msg_raw.wrench.torque.y = voltages[4];
+		msg_raw.wrench.torque.z = voltages[5];
 
-        msg_raw.header.stamp = ros::Time::now();
-        msg_raw.wrench.force.x = voltages[0];
-        msg_raw.wrench.force.y = voltages[1];
-        msg_raw.wrench.force.z = voltages[2];
-        msg_raw.wrench.torque.x = voltages[3];
-        msg_raw.wrench.torque.y = voltages[4];
-        msg_raw.wrench.torque.z = voltages[5];
+		if (device == Sensor::ARCHITECT)
+		{
+		    msg.wrench.force.x += 1.5;
+		    msg.wrench.force.y -= 6.5;
+		    msg.wrench.force.z -= 17.3;
+		}
 
-        if (device == Sensor::ARCHITECT)
-        {
-            msg.wrench.force.x += 1.5;
-            msg.wrench.force.y -= 6.5;
-        }
+		fts[device].pub.publish(msg);
+		fts[device].pub_raw.publish(msg_raw);
 
-        fts[device].pub.publish(msg);
-        fts[device].pub_raw.publish(msg_raw);
-
+		//std::cout<<"\nPublished FTS for "<<device<<"\n";
+	}
         ros::spinOnce();
         loop_rate.sleep();
     }
